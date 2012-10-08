895514ac7a774bd31d011ae61d10ca017beacef6
gavin <rtes@ubuntu.(none)>
2012-10-08 23:49:12 +0800

    modify post draft.sh

--- source/_posts/draft/post_draft.sh
+++ source/_posts/draft/post_draft.sh
@@ -6,10 +6,11 @@ mv $dir/$1 $dir/../
 git add $dir/../$1
 git commit -m "post drafts $1"
 
+cd $dir/../
 git checkout master
-git checkout drafts $dir/../$1
+git checkout drafts $dir/$1
 
-git add $dir/../$1
+git add $dir/$1
 git commit -m "post drafts $1"
 
 rake generate
