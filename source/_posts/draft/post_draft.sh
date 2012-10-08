f759fc6a2661a4d302a378ce3e9afd319bb7482b
gavin <rtes@ubuntu.(none)>
2012-10-08 23:55:54 +0800

    post drafts

--- source/_posts/draft/post_draft.sh
+++ source/_posts/draft/post_draft.sh
@@ -2,15 +2,18 @@
 
 dir=`pwd`
 
-mv $dir/$1 $dir/../
-git add $dir/../$1
+set -x
+mv $1 ../
+git add ../$1
 git commit -m "post drafts $1"
 
 cd $dir/../
-git checkout master
-git checkout drafts $dir/$1
 
-git add $dir/$1
+new_dir=`pwd`
+git checkout master
+git checkout drafts $new_dir/$1
+
+git add $new_dir/$1
 git commit -m "post drafts $1"
 
 rake generate
