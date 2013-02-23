1e9050165309fe8c1e7ce59b779419379f23e725
gavin <gavin@gavin-lab-desktop.(none)>
2013-02-23 22:19:02 +0800

    update script

--- source/_posts/post_draft.sh
+++ source/_posts/post_draft.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-git checkout drafts
+if [ $# != 3 ]
+then
+   usage: <file name> <year> <month>
+fi
 
-git mv ./draft/$1 ./
-git add $1
+if [ ! -d ./$2 ]
+then
+   mkdir $2
+fi
+
+if [ ! -d ./$2/$3 ]
+then 
+   mkdir $3
+fi
+
+git mv ./draft/$1 ./$2/$3
+git add ./$2/$3/$1
 git commit -m "post $1"
 
 git checkout master
 
-git checkout drafts $1
-git add $1
+git checkout drafts $2
+git add ./$2/$3/$1
 git commit -m "post $1"
 git push
 
